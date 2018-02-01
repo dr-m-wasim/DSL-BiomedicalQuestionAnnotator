ea1a5f7dbb3fcfd51fe6459c19b704b78787654b
Muhammad Sajjad <muhammad.sajjad@kics.edu.pk>
2018-02-01 11:09:17 +0500

    added main class

---
+++ src/main/java/pk/edu/com/kics/BiomedicalQuestionAnnotator/BiomedicalQestionAnnotator.java
@@ -0,0 +1,10 @@
+package pk.edu.com.kics.BiomedicalQuestionAnnotator;
+
+public class BiomedicalQestionAnnotator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
